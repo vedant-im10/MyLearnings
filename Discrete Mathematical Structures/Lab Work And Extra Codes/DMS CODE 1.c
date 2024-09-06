49f4a5fd9ac0fa46fc637e64493a346a512ca553
Vedant Hareshbhai Patel <73629995+vedant-im10@users.noreply.github.com>
2024-09-05 18:30:50 -0700

    Update and rename 19BCP138_PATEL VEDANT_DMS CODE NO. 1.c to DMS CODE 1.c

--- Discrete Mathematical Structures/Lab Work And Extra Codes/DMS CODE 1.c
+++ Discrete Mathematical Structures/Lab Work And Extra Codes/DMS CODE 1.c
@@ -4,7 +4,7 @@
 //  Create a program for checking of Isomorphism of two graphs.
 //
 //  Created by VEDANT on 27/10/20.
-//  Copyright © 2020 VEDANT. All rights reserved.
+//  Copyright Â© 2020 VEDANT. All rights reserved.
 //
 /* BY PATEL VEDANT H. - 19BCP138 - COMPUTER BRANCH */
 #include<stdio.h>
