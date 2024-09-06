ea0dcc86c2e038fe64c68d98671754cd7e9bfe91
Vedant Hareshbhai Patel <73629995+vedant-im10@users.noreply.github.com>
2024-09-05 17:42:09 -0700

    Update ConcreteAgg.java

--- Object Oriented Programming with Java/Lab Work And Extra Codes/Iterator Pattern Code/ConcreteAgg.java
+++ Object Oriented Programming with Java/Lab Work And Extra Codes/Iterator Pattern Code/ConcreteAgg.java
@@ -1,28 +1,21 @@
-
 public class ConcreteAgg implements Aggregate{
-	public String names[] = {"Rohit" , "Jignesh" ,"Aashvi" , "Lipi"};
+	public String names[] = {"Rohit", "Jignesh", "Aashvi", "Lipi"};
 
-	   
 	   public Iterator getIterator() {
 	      return new NameIterator();
 	   }
 
 	   class NameIterator implements Iterator {
-
 	      int index;
 
-	      
 	      public boolean hasNext() {
-	      
 	         if(index < names.length){
 	            return true;
 	         }
 	         return false;
 	      }
 
-	      
 	      public Object next() {
-	      
 	         if(this.hasNext()){
 	            return names[index++];
 	         }
