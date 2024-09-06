c7176a5ae4f39844ab351274a57e8c69067f1cea
Vedant Hareshbhai Patel <73629995+vedant-im10@users.noreply.github.com>
2024-09-05 18:17:38 -0700

    Update StringArray.java

--- Object Oriented Programming with Java/Lab Work And Extra Codes/StringArray.java
+++ Object Oriented Programming with Java/Lab Work And Extra Codes/StringArray.java
@@ -1,13 +1,8 @@
-
 public class StringArray {
-
-	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		String[] cars = {"Mercedes","Volvo", "BMW", "Ford", "Tata", "Mahindra", "Maruti"};
 		for (int i=0;i<cars.length;i++) {
             System.out.println("i = " + i + "  " + cars[i]);
 		}
 	}   
-
 }
