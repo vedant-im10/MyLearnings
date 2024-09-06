10e8b9217a34c7ff3a03a4c696d6f50ccb850802
Vedant Hareshbhai Patel <73629995+vedant-im10@users.noreply.github.com>
2024-09-05 18:04:30 -0700

    Update BreakXample.java

--- Object Oriented Programming with Java/Lab Work And Extra Codes/BreakXample.java
+++ Object Oriented Programming with Java/Lab Work And Extra Codes/BreakXample.java
@@ -1,24 +1,12 @@
-
 public class BreakXample {
-
-	
 	public static void main(String[] args) {
-		
-
-				int i,j; 
-				for(i=0;i<3;i++)
-				{  for(j=10;j<15;j++)
-				   {
-		            break; 
-							
-				     
-				   }
-				   System.out.print(i+" ");
-			
-
-		         }
-
-
-	}
-
+		int i,j; 
+		for(i=0;i<3;i++)
+		{  for(j=10;j<15;j++)
+		   {
+	    break; 	     
+		   }
+		   System.out.print(i+" ");
+	 }
+     }
 }
