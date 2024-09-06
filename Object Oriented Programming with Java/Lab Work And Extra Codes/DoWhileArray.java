ba2c430ed0e585e4b3adbe62c23af80e7e4b33d9
Vedant Hareshbhai Patel <73629995+vedant-im10@users.noreply.github.com>
2024-09-05 18:08:51 -0700

    Update DoWhileArray.java

--- Object Oriented Programming with Java/Lab Work And Extra Codes/DoWhileArray.java
+++ Object Oriented Programming with Java/Lab Work And Extra Codes/DoWhileArray.java
@@ -1,9 +1,5 @@
-
 public class DoWhileArray {
-
-
 	public static void main(String[] args) {
-
 		int arr[]={2,11,45,9};
 		//i starts with 0 as array index starts with 0
 		int i=0;
@@ -11,9 +7,5 @@ public class DoWhileArray {
 			System.out.println(arr[i]);
 			i++;
 		}while(i<4);
-
-
-
   }
-
 }
