8e9b07085293633de2ad356d6b04570b0cfec7b8
Vedant Hareshbhai Patel <73629995+vedant-im10@users.noreply.github.com>
2024-09-05 18:28:57 -0700

    Update and rename 19BCP138_PATEL VEDANT_DMS CODE NO. 7.c to DMS CODE 7.c

--- Discrete Mathematical Structures/Lab Work And Extra Codes/DMS CODE 7.c
+++ Discrete Mathematical Structures/Lab Work And Extra Codes/DMS CODE 7.c
@@ -4,7 +4,7 @@
 //  Create a program to find the Lexicographic order of an ordered rooted tree.
 //
 //  Created by VEDANT on 24/11/20.
-//  Copyright © 2020 VEDANT. All rights reserved.
+//  Copyright Â© 2020 VEDANT. All rights reserved.
 //
 /* BY PATEL VEDANT H. - 19BCP138 - COMPUTER BRANCH */
 // Header Files included for the Program
