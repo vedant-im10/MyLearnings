4f0a210020dab7da4671ecc03d177752b0aa7186
Vedant Hareshbhai Patel <73629995+vedant-im10@users.noreply.github.com>
2024-09-05 18:21:12 -0700

    Update WhileLoop.java

--- Object Oriented Programming with Java/Lab Work And Extra Codes/WhileLoop.java
+++ Object Oriented Programming with Java/Lab Work And Extra Codes/WhileLoop.java
@@ -1,18 +1,13 @@
-
 public class WhileLoop {
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
 		int iter=10;
-		
 		while(iter>0)
 		  {  
 			 System.out.println("believe in yourself");
 			 iter--;  
 		  }
 	}
-
 }
